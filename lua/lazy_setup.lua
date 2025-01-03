f6d364444a0c2dd15aaa6c196d9254ef8040d6bc
XG2JN <201910311201@stu.shmtu.edu.cn>
2025-01-03 15:07:15 +0800

    shake

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -1,26 +1,24 @@
 require("lazy").setup({
   {
     "AstroNvim/AstroNvim",
-    -- version = "^4", -- Remove version tracking to elect for nighly AstroNvim
     branch = "v5",
     import = "astronvim.plugins",
-    opts = { -- AstroNvim options must be set here with the `import` key
-      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
-      maplocalleader = " ", -- This ensures the localleader key must be configured before Lazy is set up
-      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
-      pin_plugins = true, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
-      update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
+    opts = {
+      mapleader = " ",
+      maplocalleader = " ",
+      icons_enabled = true,
+      pin_plugins = true,
+      update_notifications = true,
     },
   },
   { import = "community" },
   { import = "plugins" },
-} --[[@as LazySpec]], {
-  -- Configure any other `lazy.nvim` configuration options here
+}--[[@as LazySpec]], {
+
   install = { colorscheme = { "tokyonight-moon" } },
   ui = { backdrop = 100 },
   performance = {
     rtp = {
-      -- disable some rtp plugins, add more to your liking
       disabled_plugins = {
         "gzip",
         "netrwPlugin",
